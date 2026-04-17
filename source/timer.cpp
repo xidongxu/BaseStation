37ac0bd10542d6013278bdfee3cb33ef47c87d1e
xidongxu <xi_dongxu@126.com>
2026-04-17 11:39:01 +0800

    chore(timer): 基本的定时器功能

--- source/timer.cpp
+++ source/timer.cpp
@@ -3,7 +3,158 @@
 #include "cJSON.h"
 #include "command.h"
 #include "processor.h"
-#include "server.h"
+#include "timer.h"
 
-#define LOG_TAG "process"
+#define LOG_TAG "_timer_"
 #include "logger.h"
+
+Timer::Timer(asio::io_context& context) : m_timer(context), m_strander(asio::make_strand(context)) {}
+
+Timer::~Timer() {
+    stop();
+}
+
+void Timer::start(std::chrono::milliseconds interval, std::function<void()> callback, bool periodic) {
+    auto self = weak_from_this();
+    asio::dispatch(
+        m_strander,
+        [self, interval, callback = std::move(callback), periodic]() {
+            auto timer = self.lock();
+            if (!timer) {
+                return;
+            }
+            timer->m_interval = interval;
+            timer->m_callback = std::move(callback);
+            timer->m_periodic = periodic;
+            timer->m_running = true;
+            timer->schedule();
+        });
+}
+
+void Timer::stop() {
+    auto self = weak_from_this();
+    asio::dispatch(
+        m_strander,
+        [self]() {
+            auto timer = self.lock();
+            if (!timer) {
+                return;
+            }
+            timer->m_running = false;
+            timer->m_timer.cancel();
+        });
+}
+
+void Timer::reset() {
+    auto self = weak_from_this();
+    asio::dispatch(
+        m_strander,
+        [self]() {
+            auto timer = self.lock();
+            if (!timer) {
+                return;
+            }
+            if (!timer->m_running) {
+                return;
+            }
+            timer->m_timer.cancel();
+            timer->schedule();
+        });
+}
+
+bool Timer::running() const {
+    return m_running.load(std::memory_order_relaxed);
+}
+
+void Timer::schedule() {
+    if (!m_running)
+        return;
+    auto self = weak_from_this();
+    m_timer.expires_after(m_interval);
+    m_timer.async_wait(
+        asio::bind_executor(
+            m_strander,
+            [self](asio::error_code error) {
+                auto timer = self.lock();
+                if (!timer) {
+                    return;
+                }
+                if (error || !timer->m_running) {
+                    return;
+                }
+                if (timer->m_callback) {
+                    timer->m_callback();
+                }
+                if (timer->m_periodic) {
+                    timer->schedule();
+                } else {
+                    timer->m_running = false;
+                }
+            }
+        ));
+}
+
+TimerManager::TimerManager() {
+    m_thread = std::thread([this]() {
+        m_context.run();
+    });
+}
+
+TimerManager::~TimerManager() {
+    m_context.stop();
+    if (m_thread.joinable()) {
+        m_thread.join();
+    }
+    std::lock_guard<std::mutex> lock(m_mutex);
+    for (auto it = m_timers.begin(); it != m_timers.end(); ) {
+        auto& timer = it->second;
+        if (timer) {
+            timer->stop();
+        }
+        it = m_timers.erase(it);
+    }
+}
+
+uint64_t TimerManager::append(std::chrono::milliseconds interval, std::function<void()> callback, bool periodic) {
+    auto timer = std::make_shared<Timer>(m_context);
+    std::lock_guard<std::mutex> lock(m_mutex);
+    timer->start(interval, std::move(callback), periodic);
+    m_timers.insert({ m_counter, timer });
+    m_counter = m_counter + 1;
+    return m_counter;
+}
+
+void TimerManager::remove(uint64_t id) {
+    std::lock_guard<std::mutex> lock(m_mutex);
+    auto it = m_timers.find(id);
+    if (it != m_timers.end()) {
+        it->second->stop();
+        m_timers.erase(it);
+    }
+}
+
+void TimerManager::stop(uint64_t id) {
+    std::lock_guard<std::mutex> lock(m_mutex);
+    auto it = m_timers.find(id);
+    if (it != m_timers.end()) {
+        it->second->stop();
+    }
+}
+
+void TimerManager::reset(uint64_t id) {
+    std::lock_guard<std::mutex> lock(m_mutex);
+    auto it = m_timers.find(id);
+    if (it != m_timers.end()) {
+        it->second->reset();
+    }
+}
+
+bool TimerManager::running(uint64_t id) {
+    bool result = false;
+    std::lock_guard<std::mutex> lock(m_mutex);
+    auto it = m_timers.find(id);
+    if (it != m_timers.end()) {
+        result = it->second->running();
+    }
+    return result;
+}
--- source/timer.h
+++ source/timer.h
@@ -1,7 +1,56 @@
 ﻿#pragma once
 
-#include <array>
-#include <deque>
+#include <asio.hpp>
+#include <atomic>
+#include <chrono>
+#include <functional>
 #include <memory>
-#include <mutex>
-#include <string>
+
+class Timer : public std::enable_shared_from_this<Timer> {
+public:
+    explicit Timer(asio::io_context& context);
+    ~Timer();
+    void start(std::chrono::milliseconds interval, std::function<void()> callback, bool periodic = false);
+    void stop();
+    void reset();
+    bool running() const;
+
+private:
+    void schedule();
+
+private:
+    asio::steady_timer m_timer;
+    asio::strand<asio::io_context::executor_type> m_strander;
+    std::chrono::milliseconds m_interval{ 0 };
+    std::function<void()> m_callback{};
+    std::atomic<bool> m_running{};
+    std::atomic<bool> m_periodic{};
+};
+
+class TimerManager {
+public:
+    static TimerManager& instance() {
+        static TimerManager instance;
+        return instance;
+    }
+    uint64_t append(std::chrono::milliseconds interval, std::function<void()> callback, bool periodic = false);
+    void remove(uint64_t id);
+    void stop(uint64_t id);
+    void reset(uint64_t id);
+    bool running(uint64_t id);
+
+private:
+    TimerManager();
+    ~TimerManager();
+    TimerManager(const TimerManager&) = delete;
+    TimerManager& operator=(const TimerManager&) = delete;
+    TimerManager(TimerManager&&) = delete;
+    TimerManager& operator=(TimerManager&&) = delete;
+
+private:
+    uint64_t m_counter{};
+    std::mutex m_mutex{};
+    std::thread m_thread{};
+    asio::io_context m_context{};
+    std::unordered_map<uint64_t, std::shared_ptr<Timer>> m_timers{};
+};
